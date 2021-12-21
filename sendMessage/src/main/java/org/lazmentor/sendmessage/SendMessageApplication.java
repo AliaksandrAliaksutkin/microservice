b753346369f292a90220c021427c9359a1ffb771
Aliaksutkin_Aliaksandr <aleksutkinaliaksandr@gmail.com>
2021-12-21 05:44:47 +0300

    create module eurekaServer

--- sendMessage/src/main/java/org/lazmentor/sendmessage/SendMessageApplication.java
+++ sendMessage/src/main/java/org/lazmentor/sendmessage/SendMessageApplication.java
@@ -2,8 +2,10 @@ package org.lazmentor.sendmessage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SendMessageApplication {
 
     public static void main(String[] args) {
